f9aa76ed70df4ce2f4318f2c6e00430637483fb7
Dali Wang <daliwang@users.noreply.github.com>
2023-11-11 22:38:17 -0500

    add utility to check variable calculation using log file

---
+++ checklog.py
@@ -0,0 +1,14 @@
+with open('output.log111123', 'r') as file:
+    lines = file.readlines()
+
+Var_name = ""
+for i in range(len(lines)):
+    if lines[i].startswith("Working on varibale: "):
+        Var_name = lines[i].split(" ")[3]
+        #print("scanning ",Var_name, "at line: ", str(i))
+        
+    elif lines[i].startswith("o_data, f_data1, f_data, dst: max/min/sum"):
+        for j in range(i+1, i+4):
+            val1, val2, val3, val4 = map(float, lines[j].split())
+            if val2 != val3 or val3 != val4:
+                print("Wrong calculation of variable " + Var_name + "(at line) "+ str(j))
